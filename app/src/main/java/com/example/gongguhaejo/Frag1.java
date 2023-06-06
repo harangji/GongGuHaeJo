225b90cda2dedee09cdc59fc0129c9d01c238b49
harang <haohao44@naver.com>
2023-06-06 17:11:53 +0900

    frag1

--- app/src/main/java/com/example/gongguhaejo/Frag1.java
+++ app/src/main/java/com/example/gongguhaejo/Frag1.java
@@ -24,7 +24,7 @@ import java.util.ArrayList;
 
 public class Frag1 extends Fragment {
 
-
+//test
     private RecyclerView recyclerView;
     private CustomAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
